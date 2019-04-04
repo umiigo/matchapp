be604db8c4d1dd5ec9e38c4c009919d029efd9ca
islamoo <islamoo@Umairs-MacBook-Pro.local>
2019-04-04 15:11:05 +0100

    added panResponders for card swipe

--- App.js
+++ App.js
@@ -1,31 +1,45 @@
-import React,  {Component} from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import Expo from 'expo'
+import React, { Component } from 'react'
+import { View, StyleSheet, Image, Text, PanResponder } from 'react-native'
+
+const fbImage = 'https://graph.facebook.com/259389830744794/picture?height=500'
 
 export default class App extends Component {
+  componentWillMount() {
+    this.cardPanResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (e, gesture) => console.log(gesture.moveX),
+      onPanResponderRelease: (e, gesture) => console.log('Released', gesture.moveY),
+    })
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableHighlight style={styles.button}>
-          <Text>Match App Here</Text>
-        </TouchableHighlight>
+      <View
+        {...this.cardPanResponder.panHandlers}
+        style={styles.card}>
+        <Image
+          style={{ flex: 1 }}
+          source={{ uri: fbImage }}
+        />
+        <View style={{ margin: 20 }}>
+          <Text style={{ fontSize: 20 }}>Candice, 28</Text>
+          <Text style={{ fontSize: 15, color: 'darkgrey' }}>Supermodel</Text>
+        </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  card: {
     flex: 1,
-    backgroundColor: 'midnightblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    margin: 10,
+    marginTop: 100,
+    marginBottom: 100,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
     borderRadius: 8,
-    backgroundColor: 'deepskyblue',
-    width: 150,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center'
   }
-});
+})
